8f4b39e5f6cf1a7a0fbcada1d0d0c9f8d2146d7f
HanlaoTwo <hankeboom@163.com>
2017-03-03 19:53:23 +0800

    增加显示文件内容的directive

--- client/module1/module1.js
+++ client/module1/module1.js
@@ -1,31 +1,47 @@
 /**
-     * Created by hanqian18790 on 2017/2/14.
-     */
+ * Created by hanqian18790 on 2017/2/14.
+ */
 (function () {
     'use strict';
 
     angular
         .module('module1')
-        .controller('Module1Ctrl', Module1Ctrl);
+        .controller('Module1Ctrl', Module1Ctrl)
+        .directive('innerContent', innerContent);
+
+
+    function innerContent() {
+        return {
+            template: '{{vm.mdContent}}'
+        };
+    }
 
     function Module1Ctrl() {
         var vm = this;
-        vm.show = true;
+        vm.mdContent = "it is your content area";
+        vm.list_open = true;
+        vm.list_close = false;
         vm.hello = "lallalala";
-        vm.tree = { };
+        vm.tree = {};
         vm.listfiles = function () {
             $.ajax({
                 type: "GET",
                 url: "../cgi-bin/listFiles.py",
                 data: {},
                 dataType: "json",
-                success: function(data){
+                success: function (data) {
                     vm.tree = data;
-                    console.log(vm.tree);
+                    console.log(data.dirs[0].name);
                 }
             });
         };
         vm.listfiles();
-
+        vm.showul = function () {
+            vm.show = !vm.show;
+        }
+        vm.showBlog = function (el) {
+            console.log("lalllalallalal");
+            vm.mdContent = "hahahahahahah"
+        }
     }
 })();
